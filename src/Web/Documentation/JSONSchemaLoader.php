0259e56cd21bb8cdd67de57f4b7014f521fb610c
Stefan Graupner <stefan.graupner@gmail.com>
2016-04-05 12:00:56 +0200

    Implemented missing format for array types.

--- src/Web/Documentation/JSONSchemaLoader.php
+++ src/Web/Documentation/JSONSchemaLoader.php
@@ -2,54 +2,47 @@
 
 class JSONSchemaLoader implements Loader
 {
-  public function loadForModel($modelName)
-  {
-    try
+    public function loadForModel($modelName)
     {
-      $path = base_path(config('transfugio.web.documentationRoot'));
+        try {
+            $path = base_path(config('transfugio.web.documentationRoot'));
 
-      $json = file_get_contents(sprintf('%s/%s.json', $path, ucfirst($modelName)));
+            $json = file_get_contents(sprintf('%s/%s.json', $path, ucfirst($modelName)));
 
-      $schema = json_decode($json, true);
-    } catch (\ErrorException $e)
-    {
-      $schema = null;
-      // TODO: better error handling, like, fallback to phpdoc for instance
+            $schema = json_decode($json, true);
+        } catch (\ErrorException $e) {
+            $schema = null;
+            // TODO: better error handling, like, fallback to phpdoc for instance
+        }
+
+        $documentor = new DefaultDocumentor($schema['title'], $schema['description']);
+
+        foreach ($schema['properties'] as $fieldName => $options) {
+            $description = (isset($options['description'])) ? $options['description'] : '';
+
+            $status = FieldDocumentor::STATUS_NONE;
+            if (isset($schema['required']) && in_array($fieldName, $schema['required'])) {
+                $status = FieldDocumentor::STATUS_REQUIRED;
+            }
+
+            $cardinality = FieldDocumentor::CARDINALITY_ONE;
+            if (strtolower($options['type']) === 'array') {
+                $cardinality = FieldDocumentor::CARDINALITY_UNBOUNDED;
+            }
+
+            // TODO: other field data (specific references, ...)
+
+            $field = new DefaultFieldDocumentor($fieldName, $description, $cardinality, $status);
+
+            if (isset($options['format'])) {
+                $field->setFormat($options['format']);
+            } else if ($field->getCardinality() == FieldDocumentor::CARDINALITY_UNBOUNDED && isset($options['items']['format'])) {
+                $field->setFormat($options['items']['format']);
+            }
+
+            $documentor->addField($field);
+        }
+
+        return $documentor;
     }
-
-    $documentor = new DefaultDocumentor($schema['title'], $schema['description']);
-
-    foreach ($schema['properties'] as $fieldName => $options)
-    {
-      $description = (isset($options['description'])) ? $options['description'] : '';
-
-      $status = FieldDocumentor::STATUS_NONE;
-      if (isset($schema['required']) && in_array($fieldName, $schema['required']))
-      {
-        $status = FieldDocumentor::STATUS_REQUIRED;
-      }
-
-      $cardinality = FieldDocumentor::CARDINALITY_ONE;
-      if (strtolower($options['type']) === 'array')
-      {
-        $cardinality = FieldDocumentor::CARDINALITY_UNBOUNDED;
-      }
-
-      // TODO: other field data (specific references, ...)
-
-      $field = new DefaultFieldDocumentor($fieldName, $description, $cardinality, $status);
-
-      if (isset($options['format']))
-      {
-        $field->setFormat($options['format']);
-      } else if ($field->getCardinality() == FieldDocumentor::CARDINALITY_UNBOUNDED)
-      {
-        $field->setFormat($options['items']['format']);
-      }
-
-      $documentor->addField($field);
-    }
-
-    return $documentor;
-  }
 }
