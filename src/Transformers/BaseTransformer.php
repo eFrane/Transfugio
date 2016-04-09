95e03b57f2f008b9df879fe60bb796a15b06cc0f
Stefan Graupner <stefan.graupner@gmail.com>
2016-04-09 14:38:14 +0200

    Fluent setters.

--- src/Transformers/BaseTransformer.php
+++ src/Transformers/BaseTransformer.php
@@ -5,61 +5,100 @@ use League\Fractal\TransformerAbstract;
 
 abstract class BaseTransformer extends TransformerAbstract
 {
-  private $availableFormatters = [];
-  private $loadedFormatters = [];
+    private $availableFormatters = [];
+    private $loadedFormatters = [];
 
-  private $isIncluding = false;
+    private $included = false;
 
-  public function __construct($isIncluding = false)
-  {
-    $this->availableFormatters = config('transfugio.transformers.formatHelpers');
-    $this->setIncluding($isIncluding);
-  }
-
-  public function isIncluding()
-  {
-    return $this->isIncluding;
-  }
-
-  public function setIncluding($isIncluding)
-  {
-    $this->isIncluding = $isIncluding;
-  }
-
-  /**
-   * Get a route list (object list) from a Collection
-   *
-   * @param string $routeName
-   * @param Collection $collection
-   * @param string $key
-   * @return array
-   **/
-  protected function collectionRouteList($routeName, Collection $collection, $key = 'id')
-  {
-    $keys = $collection->lists($key);
-
-    return array_map(function ($key) use ($routeName) {
-      return route($routeName, $key);
-    }, $keys->all());
-  }
-
-  public function __call($formatHelperMethod, array $value)
-  {
-    if (substr($formatHelperMethod, 0, 6) !== 'format')
-      throw new \LogicException("Format helper {$formatHelperMethod} is invalid");
-
-    $formatHelperName = strtolower(substr($formatHelperMethod, 6));
-
-    if (array_key_exists($formatHelperName, $this->availableFormatters))
+    public function __construct($included = false)
     {
-      // load the format helper
-      if (!array_key_exists($formatHelperName, $this->loadedFormatters))
-        $this->loadedFormatters[$formatHelperName] = new $this->availableFormatters[$formatHelperName];
-
-      // check for null value
-      if (is_null($value[0])) return null;
-
-      return $this->loadedFormatters[$formatHelperName]->format($value[0]);
+        $this->availableFormatters = config('transfugio.transformers.formatHelpers');
     }
-  }
+
+    /**
+     * @return array
+     */
+    public function getLoadedFormatters()
+    {
+        return $this->loadedFormatters;
+    }
+
+    /**
+     * @param array $loadedFormatters
+     */
+    public function setLoadedFormatters($loadedFormatters)
+    {
+        $this->loadedFormatters = $loadedFormatters;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isIncluded()
+    {
+        return $this->included;
+    }
+
+    /**
+     * @param boolean $included
+     */
+    public function setIncluded($included)
+    {
+        $this->included = $included;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAvailableFormatters()
+    {
+        return $this->availableFormatters;
+    }
+
+    /**
+     * @param array $availableFormatters
+     * @return BaseTransformer
+     */
+    public function setAvailableFormatters($availableFormatters)
+    {
+        $this->availableFormatters = $availableFormatters;
+
+        return $this;
+    }
+
+    public function __call($formatHelperMethod, array $value)
+    {
+        if (substr($formatHelperMethod, 0, 6) !== 'format')
+            throw new \LogicException("Format helper {$formatHelperMethod} is invalid");
+
+        $formatHelperName = strtolower(substr($formatHelperMethod, 6));
+
+        if (array_key_exists($formatHelperName, $this->availableFormatters)) {
+            // load the format helper
+            if (!array_key_exists($formatHelperName, $this->loadedFormatters))
+                $this->loadedFormatters[$formatHelperName] = new $this->availableFormatters[$formatHelperName];
+
+            // check for null value
+            if (is_null($value[0])) return null;
+
+            return $this->loadedFormatters[$formatHelperName]->format($value[0]);
+        }
+    }
+
+    /**
+     * Get a route list (object list) from a Collection
+     *
+     * @param string $routeName
+     * @param Collection $collection
+     * @param string $key
+     * @return array
+     **/
+    protected function collectionRouteList($routeName, Collection $collection, $key = 'id')
+    {
+        $keys = $collection->pluck($key);
+
+        return array_map(function ($key) use ($routeName) {
+            return route($routeName, $key);
+        }, $keys->all());
+    }
 }
