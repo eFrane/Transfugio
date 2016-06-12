213da231903bda9a2144cecf796d09a21941e579
Stefan Graupner <stefan.graupner@gmail.com>
2016-06-12 11:55:14 +0200

    Some more registry code.

--- src/Http/ControllerRegistry.php
+++ src/Http/ControllerRegistry.php
@@ -1,6 +1,5 @@
-<?php namespace EFrane\Transfugio;
+<?php namespace EFrane\Transfugio\Http;
 
-use EFrane\Transfugio\Http\APIController;
 use Illuminate\Support\Collection;
 
 class ControllerRegistry
@@ -10,6 +9,14 @@ class ControllerRegistry
      */
     protected $controllers = null;
 
+    /*
+     * array[string] mapping of controller method keywords to http verb
+     */
+    protected $verbs = [
+        'index' => ['GET', 'HEAD'],
+        'show'  => ['GET', 'HEAD'],
+    ];
+
     public function __construct()
     {
         $this->controllers = new Collection();
@@ -17,11 +24,26 @@ class ControllerRegistry
 
     public function push(APIController $controller)
     {
-        $this->controllers->push($controller);
+        $this->controllers->put(get_class($controller), $controller);
     }
 
-    public function registerRoutes()
+    public function getRoutes()
     {
-        
+        $verbs = collect($this->verbs);
+
+        return $this->controllers->flatMap(function (APIController $controller) {
+            // map controllers to arrays of controller functions => http method
+            $reflectionClass = new \ReflectionClass($controller);
+
+            return $reflectionClass->getMethods(\ReflectionMethod::IS_PUBLIC);
+        })->flatMap(function (\ReflectionMethod $method) use ($verbs) {
+            $verbs->map(function ($http, $controller) use ($method) {
+                if (starts_with($method->getName(), $controller)) {
+                    return [$http, $method];
+                }
+
+                return [];
+            });
+        }); // TODO: actually get the real routes
     }
 }
