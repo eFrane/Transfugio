a622c34ef3c8d768a7d31a70c37e7917da66cf45
Stefan Graupner <stefan.graupner@gmail.com>
2016-12-29 02:43:27 +0100

    Fix date formatter

--- src/Transformers/Formatter/DateISO8601.php
+++ src/Transformers/Formatter/DateISO8601.php
@@ -7,7 +7,7 @@ class DateISO8601 implements FormatHelper
     public function format($value)
     {
         if ($value instanceof Carbon) {
-            return $value->format('%Y-%m-%d');
+            return $value->format('Y-m-d');
         } else {
             throw new \InvalidArgumentException("`$value` is not of type Carbon\\Carbon");
         }
--- tests/unit/Transformers_Formatter_DateISO8601Test.php
+++ tests/unit/Transformers_Formatter_DateISO8601Test.php
@@ -1,7 +1,7 @@
 <?php
 
 use Carbon\Carbon;
-use EFrane\Transfugio\Transformers\Formatter\DateTimeISO8601;
+use EFrane\Transfugio\Transformers\Formatter\DateISO8601;
 
 class Transformers_Formatter_DateISO8601Test extends \Codeception\TestCase\Test
 {
@@ -12,17 +12,17 @@ class Transformers_Formatter_DateISO8601Test extends \Codeception\TestCase\Test
 
   public function testInstantiate()
   {
-    $this->assertInstanceOf('EFrane\Transfugio\Transformers\Formatter\DateISO8601', new DateTimeISO8601());
+    $this->assertInstanceOf('EFrane\Transfugio\Transformers\Formatter\DateISO8601', new DateISO8601());
   }
 
   public function testIsFormatHelper()
   {
-    $this->assertInstanceOf('EFrane\Transfugio\Transformers\Formatter\FormatHelper', new DateTimeISO8601());
+    $this->assertInstanceOf('EFrane\Transfugio\Transformers\Formatter\FormatHelper', new DateISO8601());
   }
 
   public function testFormat()
   {
-    $formatter = new DateTimeISO8601();
+    $formatter = new DateISO8601();
 
     $this->assertEquals('2016-01-01', $formatter->format(Carbon::parse('2016-01-01')));
   }
@@ -32,7 +32,7 @@ class Transformers_Formatter_DateISO8601Test extends \Codeception\TestCase\Test
    */
   public function testFormatNonCarbon()
   {
-    $formatter = new DateTimeISO8601();
+    $formatter = new DateISO8601();
 
     $this->assertEquals('', $formatter->format('some non-date value'));
   }
